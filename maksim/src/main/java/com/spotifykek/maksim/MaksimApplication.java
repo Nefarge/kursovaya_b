183a1eee1937835aa076c5a1bea29b195d09cc60
Nefarge <79088471+Nefarge@users.noreply.github.com>
2021-05-04 23:50:22 +0300

    Add files via upload

---
+++ maksim/src/main/java/com/spotifykek/maksim/MaksimApplication.java
@@ -0,0 +1,13 @@
+package com.spotifykek.maksim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaksimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaksimApplication.class, args);
+	}
+
+}
---
+++ maksim/src/test/java/com/spotifykek/maksim/MaksimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spotifykek.maksim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaksimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
